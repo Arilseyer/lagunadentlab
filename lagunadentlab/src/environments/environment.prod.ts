b470778dfb7fb23c62a63c5797a4c624128aa43b
Fanny <reyestefania145@hotmail.com>
2025-11-10 19:28:23 -0600

    Fanny: environment correcto

--- lagunadentlab/src/environments/environment.prod.ts
+++ lagunadentlab/src/environments/environment.prod.ts
@@ -3,16 +3,6 @@ export const environment = {
 };
 
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "lagunadentlab.firebaseapp.com",
-  projectId: "lagunadentlab",
-  storageBucket: "lagunadentlab.firebasestorage.app",
-  messagingSenderId: "1038112673825",
-  appId: "1:1038112673825:web:19d945337c52a61adf5360",
-  measurementId: "G-SCQX5Z7JDP"
-};
-
-export const firebaseConfig = {
   apiKey: "AIzaSyC8Xc-cLJdvm-CC5zyy_V_wlAWxZpb4yP4",
   authDomain: "lagunadentlab.firebaseapp.com",
   projectId: "lagunadentlab",
